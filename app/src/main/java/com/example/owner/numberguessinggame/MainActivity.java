ecebe1d7c6c1618d9fb82709bb82d1259f1aae3f
RyanChang <ftes90015@gmail.com>
2017-11-16 02:51:26 +0800

    開始猜

--- app/src/main/java/com/example/owner/numberguessinggame/MainActivity.java
+++ app/src/main/java/com/example/owner/numberguessinggame/MainActivity.java
@@ -11,7 +11,9 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     int a[];
+    int notguess[];
     int ans[] = new int[2];
+    int guess[] = new int[2];
     ListView itemsListView;
     TextView textAns;
     ArrayList<Answer> list;
@@ -62,7 +64,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    String StartGuessing(int[] a,int length){
+        notguess = new int[length];
+        guess[0] = notguess[(int)(Math.random()*(length))];
+        guess[1] =notguess[(int)(Math.random()*(length))];
+        while (ans[1] == ans[0]){
+            ans[1] =notguess[(int)(Math.random()*(length))];
+        }
+        if (guess[0]!=ans[0]&&guess[1]!=ans[1]){
 
+            return StartGuessing(a,length-2);
+        }else {
+            return "我猜是:"+String.valueOf(guess[0])+" "+String.valueOf(guess[1])+"";
+        }
+
+    }
 
 
 }
