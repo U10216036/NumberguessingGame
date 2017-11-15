d26aa8f6f39511d5220f2e94865c78f0e9b5e70e
zhangxiurui <ftes90015@gmail.com>
2017-11-15 16:09:48 +0800

    剩下猜測的部分沒寫

--- app/src/main/java/com/example/owner/numberguessinggame/MainActivity.java
+++ app/src/main/java/com/example/owner/numberguessinggame/MainActivity.java
@@ -5,32 +5,64 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    int a[];
+    int ans[] = new int[2];
+    ListView itemsListView;
+    TextView textAns;
+    ArrayList<Answer> list;
+    Listviewadapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn = (Button)findViewById(R.id.btn);
-        final ListView itemsListView  = (ListView)findViewById(R.id.listView);
-        final ArrayList<Answer> list = new ArrayList<>();
-
-        final Listviewadapter adapter = new Listviewadapter(this, list);
+        textAns = (TextView)findViewById(R.id.textAns);
+        itemsListView  = (ListView)findViewById(R.id.listView);
+        list = new ArrayList<>();
+        adapter = new Listviewadapter(this, list);
         itemsListView.setAdapter(adapter);
+
+
+
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //do something
-                list.add(new Answer("XD"));
-                list.add(new Answer("22"));
+                a = new int[5];
+                for(int i =0;i < a.length;i++){
+                    a[i] = i+1;
+                }
+                generateRandomAns();
+
+                for(int i =0;i < a.length;i++){
+                    list.add(new Answer(String.valueOf(a[i])));
+                }
+
                 adapter.notifyDataSetChanged();
             }
         });
     }
+
+    void generateRandomAns(){
+
+        ans[0] = (int)(Math.random()*(a.length)+1);
+        ans[1] =(int)(Math.random()*(a.length)+1);
+        while (ans[1] == ans[0]){
+            ans[1] =(int)(Math.random()*(a.length)+1);
+        }
+
+        for (int k=0;k<ans.length;k++){
+            textAns.append(String.valueOf(ans[k] + " "));
+        }
+    }
+
+
 
 
 }
